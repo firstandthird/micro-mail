713a56534fc168163021cd01dec95ca94f643129
Eric Willis <admin@ecwillis.com>
2016-06-06 17:31:57 +0000

    Adding template view

--- routes/list.js
+++ routes/list.js
@@ -1,9 +1,24 @@
 'use strict';
 
+const fs = require('fs');
+const _ = require('lodash');
+
 exports.list = {
   path: '/',
   method: 'GET',
   handler(request, reply) {
-    reply('ok');
+    const templateDir = request.server.settings.app.views.path;
+    fs.readdir(templateDir, (err, files) => {
+      if (err) {
+        return reply(err);
+      }
+
+      files = _.map(files, (file) => {
+        const link = `<a href="/view/${file}">${file}</a>`;
+        return link;
+      });
+
+      reply(files.join('<br/>'));
+    });
   }
 };
