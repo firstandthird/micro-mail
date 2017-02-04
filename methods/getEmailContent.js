f2ae5443c0a5ed2656bc3fef16c06246d78e3a27
Chris <orthagonal1@gmail.com>
2017-02-04 13:51:45 -0600

    add juice

--- methods/getEmailContent.js
+++ methods/getEmailContent.js
@@ -2,6 +2,7 @@
 const handlebars = require('handlebars');
 const fs = require('fs');
 const path = require('path');
+const juice = require('juice');
 
 module.exports = function(templateName, data, allDone) {
   if (templateName) {
@@ -10,7 +11,7 @@ module.exports = function(templateName, data, allDone) {
       if (err) {
         return allDone(err);
       }
-      allDone(null, handlebars.compile(fileContent.toString())(data));
+      allDone(null, juice(handlebars.compile(fileContent.toString())(data)));
     });
   } else {
     return allDone(null, false);
