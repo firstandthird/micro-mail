34b5bb42dea2ddea847e3ee4f112269f0d73f164
Chris <orthagonal1@gmail.com>
2017-02-16 13:05:47 -0600

    plugins.render handles loading partials and helpers

--- methods/getEmailContent.js
+++ methods/getEmailContent.js
@@ -1,23 +1,8 @@
 'use strict';
-const handlebars = require('handlebars');
-const fs = require('fs');
-const path = require('path');
-const juice = require('juice');
 
 module.exports = function(templateName, data, allDone) {
   if (templateName) {
-    const templatePath = path.join(this.settings.app.views.path, templateName, 'email.html');
-    fs.readFile(templatePath, (err, fileContent) => {
-      if (err) {
-        return allDone(err);
-      }
-      let compiledResult = handlebars.compile(fileContent.toString())(data);
-      if (data.inlineCss) {
-        compiledResult = juice(compiledResult);
-      }
-      return allDone(null, compiledResult);
-    });
-  } else {
-    return allDone(null, false);
+    return this.methods.renderEmailTemplate(templateName, data, allDone);
   }
+  return allDone(null, false);
 };
