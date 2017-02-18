51848420971b78f7dfe47d4a7072be3b94822484
orthagonal <orthagonal1@gmail.com>
2017-02-18 09:58:40 -0600

    use vision instead of handlebars (#30)
    * use viision instead of handlebars
    
    * add test template for partials / helpers
    
    * add test helper
    
    * add test partial / update expected output
    
    * use server.render instead of handlebars

--- methods/getEmailContent.js
+++ methods/getEmailContent.js
@@ -1,17 +1,14 @@
 'use strict';
-const handlebars = require('handlebars');
-const fs = require('fs');
 const path = require('path');
 const juice = require('juice');
 
 module.exports = function(templateName, data, allDone) {
   if (templateName) {
-    const templatePath = path.join(this.settings.app.views.path, templateName, 'email.html');
-    fs.readFile(templatePath, (err, fileContent) => {
-      if (err) {
-        return allDone(err);
+    const templatePath = path.join(templateName, 'email.html');
+    this.root.render(templatePath, data, (renderErr, compiledResult) => {
+      if (renderErr) {
+        return allDone(renderErr);
       }
-      let compiledResult = handlebars.compile(fileContent.toString())(data);
       if (data.inlineCss) {
         compiledResult = juice(compiledResult);
       }
