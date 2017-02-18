1558549f2d8a5f3726f47c6e41cf2faebc154c32
Chris <orthagonal1@gmail.com>
2017-02-18 09:34:41 -0600

    use server.render instead of handlebars

--- methods/getEmailContent.js
+++ methods/getEmailContent.js
@@ -1,25 +1,18 @@
 'use strict';
-const fs = require('fs');
 const path = require('path');
 const juice = require('juice');
 
 module.exports = function(templateName, data, allDone) {
   if (templateName) {
-    const templatePath = path.join(this.settings.app.views.path, templateName, 'email.html');
-    fs.readFile(templatePath, (err, fileContent) => {
-      if (err) {
-        return allDone(err);
+    const templatePath = path.join(templateName, 'email.html');
+    this.root.render(templatePath, data, (renderErr, compiledResult) => {
+      if (renderErr) {
+        return allDone(renderErr);
       }
-      // let compiledResult = handlebars.compile(fileContent.toString())(data);
-      this.render(fileContent.toString(), data, (renderErr, compiledResult) => {
-        if (renderErr) {
-          return allDone(renderErr);
-        }
-        if (data.inlineCss) {
-          compiledResult = juice(compiledResult);
-        }
-        return allDone(null, compiledResult);
-      });
+      if (data.inlineCss) {
+        compiledResult = juice(compiledResult);
+      }
+      return allDone(null, compiledResult);
     });
   } else {
     return allDone(null, false);
