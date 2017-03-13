9fc533f3756a3c6d9d5004f7d43f5065471edf04
Chris <orthagonal1@gmail.com>
2017-03-13 18:12:17 -0500

    sendMail.test -> tap

--- test/method.sendMail.test.js
+++ test/method.sendMail.test.js
@@ -1,13 +1,59 @@
 'use strict';
-const test = require('./loadTests.js');
+const tap = require('tap');
+const Rapptor = require('rapptor');
+const SMTPServer = require('smtp-server').SMTPServer;
+const path = require('path');
 
-test('sendEmail - accepts valid submission and envelope', (assert, servers) => {
+let rapptor;
+let server;
+let smtpServer;
+tap.beforeEach((done) => {
+  rapptor = new Rapptor();
+  const onData = (stream, session, callback) => {
+    stream.on('end', () => callback(null, 'Message queued'));
+    stream.on('data', () => {});
+  };
+  // set up a test smtp server:
+  smtpServer = new SMTPServer({
+    // uncomment to show SMTP exchange:
+    // logger: true,
+    disabledCommands: ['STARTTLS'],
+    // auth method, for testing just needs to verify that
+    // login info was passed correctly
+    onAuth: (auth, session, callback) => callback(null, {
+      user: auth.username,
+      password: auth.password
+    }),
+    socketTimeout: 100 * 1000,
+    closeTimeout: 6 * 1000,
+    onData
+  });
+  smtpServer.listen(8888, 'localhost');
+  rapptor.start((err, returned) => {
+    if (err) {
+      return done(err);
+    }
+    server = returned;
+    server.settings.app.views.path = path.join(__dirname, 'emails');
+    done();
+  });
+});
+
+tap.afterEach((done) => {
+  rapptor.stop(() => {
+    smtpServer.close(() => {
+      done();
+    });
+  });
+});
+
+tap.test('sendEmail - accepts valid submission and envelope', (assert) => {
   const mailObj = {
     from: 'someone@somewhere.com',
     to: 'nobody@nowhere.com',
     html: 'some html'
   };
-  servers.server.methods.sendMail(mailObj, false, (err, result) => {
+  server.methods.sendMail(mailObj, false, (err, result) => {
     assert.equal(err, null);
     assert.equal(typeof result, 'object', 'returns an object');
     assert.equal(result.accepted[0], 'nobody@nowhere.com', 'accepted result is correct');
@@ -18,11 +64,11 @@ test('sendEmail - accepts valid submission and envelope', (assert, servers) => {
   });
 });
 
-test('sendEmail - accepts valid submission', (assert, servers) => {
+tap.test('sendEmail - accepts valid submission', (assert) => {
   const mailObj = {
     to: 'nobody@nowhere.com'
   };
-  servers.server.methods.sendMail(mailObj, false, (err, result) => {
+  server.methods.sendMail(mailObj, false, (err, result) => {
     assert.equal(err, null, 'does not error');
     assert.equal(typeof result, 'object', 'returns an object');
     assert.equal(result.accepted[0], 'nobody@nowhere.com', 'accepted result is correct');
@@ -32,23 +78,23 @@ test('sendEmail - accepts valid submission', (assert, servers) => {
   });
 });
 
-test('sendEmail - accepts rejects invalid submission without destination', (assert, servers) => {
+tap.test('sendEmail - accepts rejects invalid submission without destination', (assert) => {
   const mailObj = {
     from: 'someone@somewhere.com'
   };
-  servers.server.methods.sendMail(mailObj, false, (err, result) => {
+  server.methods.sendMail(mailObj, false, (err, result) => {
     assert.notEqual(err, null, 'throws error if invalid destination');
     assert.end();
   });
 });
 
-test('should be able to send multiple destination emails at once as a comma-separated string', (assert, servers) => {
+tap.test('should be able to send multiple destination emails at once as a comma-separated string', (assert) => {
   const mailObj = {
     from: 'eagles@nest.com',
     to: 'prey@river.com, fish@lake.com',
     subject: 'What times are you available later?'
   };
-  servers.server.methods.sendMail(mailObj, false, (err, result) => {
+  server.methods.sendMail(mailObj, false, (err, result) => {
     assert.equal(err, null);
     assert.equal(result.accepted.length, 2, 'all emails accepted');
     assert.equal(result.response, '250 Message queued');
@@ -57,13 +103,13 @@ test('should be able to send multiple destination emails at once as a comma-sepa
   });
 });
 
-test('should be able to send multiple destination emails at once as an array of strings', (assert, servers) => {
+tap.test('should be able to send multiple destination emails at once as an array of strings', (assert) => {
   const mailObj = {
     from: 'eagles@nest.com',
     to: ['prey@river.com', 'fish@lake.com'],
     subject: 'What times are you available later?',
   };
-  servers.server.methods.sendMail(mailObj, false, (err, result) => {
+  server.methods.sendMail(mailObj, false, (err, result) => {
     assert.equal(err, null);
     assert.equal(result.accepted.length, 2, '');
     assert.equal(result.response, '250 Message queued');
@@ -72,13 +118,13 @@ test('should be able to send multiple destination emails at once as an array of 
   });
 });
 
-test('will send separate emails to several destinations ', (assert, servers) => {
+tap.test('will send separate emails to several destinations ', (assert) => {
   const mailObj = {
     to: 'prey@river.com, vultures@largetree.com, crows@rock.com',
     from: 'emal@example.com',
     subject: 'This is a subject'
   };
-  servers.server.methods.sendMail(mailObj, true, (err, result) => {
+  server.methods.sendMail(mailObj, true, (err, result) => {
     assert.equal(err, null);
     assert.equal(result.length, 3);
     result.forEach((singleResult) => {
@@ -90,13 +136,13 @@ test('will send separate emails to several destinations ', (assert, servers) => 
   });
 });
 
-test('will return if any email fails and list status for specific emails', (assert, servers) => {
+tap.test('will return if any email fails and list status for specific emails', (assert) => {
   const mailObj = {
     to: 'prey@river.com, notanaddress, crows@rock.com',
     from: 'emal@example.com',
     subject: 'This is a subject'
   };
-  servers.server.methods.sendMail(mailObj, true, (err, result) => {
+  server.methods.sendMail(mailObj, true, (err, result) => {
     assert.equal(err, null, 'does not throw an error');
     assert.equal(result.length, 3);
     assert.equal(result[0].response, '250 Message queued');
