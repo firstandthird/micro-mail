f9687322939ba6173e694ff541cb2a38ab468aee
Chris <orthagonal1@gmail.com>
2017-03-13 17:33:48 -0500

    fixing in progress

--- test/loadTests.js
+++ test/loadTests.js
@@ -1,24 +1,50 @@
 'use strict';
 const setup = require('./setup');
-const tapeExtra = require('tape-extras');
-const tape = require('tape');
+const tap = require('tap');
 
-const test = tapeExtra(tape, {
-  before(done) {
-    setup({}, (setupError, server, smtpServer) => {
+// const tapeExtra = require('tape-extras');
+// const tape = require('tape');
+//
+// const test = tapeExtra(tape, {
+//   before(done) {
+//     });
+//   },
+//   after(servers, done) {
+//     servers.server.stop(() => {
+//       servers.smtpServer.close(() => {
+//         done();
+//       });
+//     });
+//   }
+// });
+
+let count = 0;
+let server = false;
+let smtpServer = false;
+module.exports = (testName, done) => {
+  const assert = tap.test;
+  const tempEnd = assert.end;
+  assert.end = () => {
+    count--;
+    if (count === 0) {
+      server.stop(() => {
+        smtpServer.close(() => {
+          tempEnd();
+        });
+      });
+    }
+  };
+  count++;
+  if (count === 1) {
+    setup({}, (setupError, returnedServer, returnedSmtpServer) => {
       if (setupError) {
         throw setupError;
       }
-      done(null, { server, smtpServer });
-    });
-  },
-  after(servers, done) {
-    servers.server.stop(() => {
-      servers.smtpServer.close(() => {
-        done();
-      });
+      server = returnedServer;
+      smtpServer = returnedSmtpServer;
+      console.log('start')
+      console.log('send gback')
+      done(assert, { server, smtpServer });
     });
   }
-});
-
-module.exports = test;
+};
--- test/method.getMailObject.test.js
+++ test/method.getMailObject.test.js
@@ -1,57 +1,87 @@
 'use strict';
-const test = require('./loadTests.js');
+const tap = require('tap');
+const Rapptor = require('rapptor');
+const path = require('path');
 
-test('getMailObject ', (assert, servers) => {
-  const details = {
-    from: 'putin@kremlin.ru',
-    fromName: 'crabbe',
-    to: 'donald@whitehouse.gov',
-    subject: 'that thing we discussed',
-    text: 'we should do it next week they will never see it coming'
+let rapptor;
+let server;
+let smtpServer;
+tap.beforeEach((done) => {
+  rapptor = new Rapptor();
+  const onData = (stream, session, callback) => {
+    stream.on('end', () => callback(null, 'Message queued'));
+    stream.on('data', () => {});
   };
-  const content = 'do that thing we discussed last week';
-  servers.server.methods.getMailObject(details, content, (err, result) => {
-    assert.equal(err, null, 'getMailObject works');
-    assert.equal(typeof result, 'object', 'getMailObject result is object');
-    assert.deepEqual(result, {
-      to: details.to,
-      from: '"crabbe" <putin@kremlin.ru>',
-      subject: details.subject,
-      html: content,
-      text: details.text
-    }, 'getMailObject result filled out correctly');
-    assert.end();
+  rapptor.start((err, returned) => {
+    if (err) {
+      return done(err);
+    }
+    server = returned;
+    server.settings.app.views.path = path.join(__dirname, 'emails');
+    done();
   });
 });
 
-test('getMailObject will not validate if missing required fields', (assert, servers) => {
-  const details = {
-    fromName: 'crabbe',
-    text: 'we should do it next week they will never see it coming'
-  };
-  const content = 'do that thing we discussed last week';
-  servers.server.methods.getMailObject(details, content, (err, result) => {
-    assert.notEqual(err, null, 'getMailObject does not validate if missing fields');
-    assert.equal(err.name, 'ValidationError', 'throws validation error');
-    assert.end();
+tap.afterEach((done) => {
+  rapptor.stop(() => {
+    done();
   });
 });
 
-test('getMailObject --with headers ', (assert, servers) => {
-  const details = {
-    headers: {
-      'the-secret-number': '206'
-    },
-    fromEmail: 'putin@kremlin.ru',
-    fromName: 'crabbe',
-    to: 'donald@whitehouse.gov',
-    subject: 'that thing we discussed',
-    text: 'we should do it next week they will never see it coming'
-  };
-  const content = 'do that thing we discussed last week';
-  servers.server.methods.getMailObject(details, content, (err, result) => {
-    assert.equal(err, null, 'getMailObject with headers, no error');
-    assert.equal(result.headers['the-secret-number'], '206', 'passes headers');
-    assert.end();
-  });
+tap.test('getMailObject ', (assert) => {
+  // console.log(Object.keys(assert))
+  assert.done();
 });
+//   const details = {
+//     from: 'putin@kremlin.ru',
+//     fromName: 'crabbe',
+//     to: 'donald@whitehouse.gov',
+//     subject: 'that thing we discussed',
+//     text: 'we should do it next week they will never see it coming'
+//   };
+//   const content = 'do that thing we discussed last week';
+//   servers.server.methods.getMailObject(details, content, (err, result) => {
+//     assert.equal(err, null, 'getMailObject works');
+//     assert.equal(typeof result, 'object', 'getMailObject result is object');
+//     assert.deepEqual(result, {
+//       to: details.to,
+//       from: '"crabbe" <putin@kremlin.ru>',
+//       subject: details.subject,
+//       html: content,
+//       text: details.text
+//     }, 'getMailObject result filled out correctly');
+//     assert.end();
+//   });
+// });
+//
+// test('getMailObject will not validate if missing required fields', (assert, servers) => {
+//   const details = {
+//     fromName: 'crabbe',
+//     text: 'we should do it next week they will never see it coming'
+//   };
+//   const content = 'do that thing we discussed last week';
+//   servers.server.methods.getMailObject(details, content, (err, result) => {
+//     assert.notEqual(err, null, 'getMailObject does not validate if missing fields');
+//     assert.equal(err.name, 'ValidationError', 'throws validation error');
+//     assert.end();
+//   });
+// });
+//
+// test('getMailObject --with headers ', (assert, servers) => {
+//   const details = {
+//     headers: {
+//       'the-secret-number': '206'
+//     },
+//     fromEmail: 'putin@kremlin.ru',
+//     fromName: 'crabbe',
+//     to: 'donald@whitehouse.gov',
+//     subject: 'that thing we discussed',
+//     text: 'we should do it next week they will never see it coming'
+//   };
+//   const content = 'do that thing we discussed last week';
+//   servers.server.methods.getMailObject(details, content, (err, result) => {
+//     assert.equal(err, null, 'getMailObject with headers, no error');
+//     assert.equal(result.headers['the-secret-number'], '206', 'passes headers');
+//     assert.end();
+//   });
+// });
