8d41141a7de243bc39cca0ba08ca4e9b889e0179
Eric Willis <eric@ecwillis.com>
2017-06-21 15:05:37 +0000

    Adding new files

---
+++ plugins/helpers/trackClick.js
@@ -0,0 +1,42 @@
+const aug = require('aug');
+const qs = require('querystring');
+
+module.exports = function(url, opts, done) {
+  const server = this.server;
+  const options = this.options;
+
+  if (!options.clicks || !options.clicks.enabled || !options.trackingUrl ) {
+    return done(null, url);
+  }
+  
+  if (!done) {
+    done = opts;
+    opts = {};
+  }
+  
+  const params = {};
+
+  // Generate Tags
+  const tagList = []; 
+  if (options.clicks.tags) {
+    tagList.push(options.clicks.tags);
+  }
+
+  if (opts.tags) {
+    tagList.push(opts.tags);
+  }
+
+  const allOpts = aug(options.clicks, opts);
+  delete allOpts.enabled;
+  
+  allOpts.tags = tagList.join(',');
+ 
+  const paramStr = [];
+  Object.keys(allOpts).forEach(p => {
+    paramStr.push(`${p}=${qs.escape(allOpts[p])}`);
+  });
+
+  const link = `${options.trackingUrl}/r/?to=${url}&${paramStr.join('&')}`;
+
+  done(null, link); 
+};
---
+++ plugins/loadHelpers.js
@@ -0,0 +1,26 @@
+
+exports.register = function(server, options, next) {
+  server.ext({
+    type: 'onPostStart',
+    method(serv, done) {
+      const viewManager = serv.root.realm.plugins.vision.manager;
+      const helpers = require('require-all')(`${__dirname}/helpers`);
+      
+      Object.keys(helpers).forEach(prop => {
+        const fn = helpers[prop].bind({
+          server: serv,
+          options
+        });
+        viewManager.registerHelper(prop, fn);
+      });
+
+      done();
+    }
+  });
+
+  next();
+};
+
+exports.register.attributes = {
+  name: 'app-helpers'
+};
