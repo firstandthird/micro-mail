c3ea5c96e0789be0897c3dd27d55e5d4ef3976a0
Eric Willis <eric@ecwillis.com>
2017-07-14 06:46:31 +0000

    Adding to send obj too.

--- routes/send.js
+++ routes/send.js
@@ -18,7 +18,8 @@ exports.send = {
         template: Joi.string(),
         subject: Joi.string(),
         pagedata: Joi.string(),
-        trackingData: Joi.object().optional()
+        trackingData: Joi.object().optional(),
+        disableTracking: Joi.bool().optional()
       }).or('text', 'template', 'pagedata')
     }
   },
