fc6b97d40ae1ef511f0a24ff44dda1470d485bb9
Troy Whiteley <troy@somany.us>
2019-05-22 03:00:00 -0700

    more debug logging

--- methods/getEmailContent.js
+++ methods/getEmailContent.js
@@ -3,12 +3,17 @@ const path = require('path');
 const juice = require('juice');
 
 module.exports = async function(templateName, data) {
+  const server = this;
   if (templateName) {
     const templatePath = path.join(templateName, 'email.njk');
+    server.log(['info', 'getEmailContent'], { templatePath });
     let compiledResult = await this.render(templatePath, data);
+    server.log(['info', 'getEmailContent'], { compiledResult });
     if (data.inlineCss) {
       compiledResult = juice(compiledResult);
     }
+
+    server.log(['info', 'getEmailContent'], { juiced: compiledResult });
     return compiledResult;
   }
   return false;
